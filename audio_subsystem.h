c1720fa1386fdfb972990deff92181cf716fc44c
Your Name <you@example.com>
2022-05-31 19:12:39 +0200

    Move audio subsysytem to separate files.

---
+++ audio_subsystem.h
@@ -0,0 +1,9 @@
+#ifndef __AUDIO_SUBSYSTEM_H__
+#define __AUDIO_SUBSYSTEM_H__
+
+#define USE_AUDIO_I2S 1
+#include "audio_i2s.h"
+
+audio_buffer_pool_t *init_audio();
+
+#endif // __AUDIO_SUBSYSTEM_H__
